0d38158ddac64bde07d2fd3edd61779b49f29ee6
Shaw <lsk.boy.f@gmail.com>
2019-09-28 00:36:13 +0800

    新增冒泡排序算法

---
+++ src/main/java/sort/BubbleSort.java
@@ -0,0 +1,20 @@
+package sort;
+
+public class BubbleSort {
+    public static int[] sort(int[] data) {
+        if (data == null || data.length == 1) {
+            return data;
+        }
+
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data.length - i - 1; j++) {
+                if (data[j] > data[j + 1]) {
+                    int temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+            }
+        }
+        return data;
+    }
+}
