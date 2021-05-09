353494ed6d32226aaaa8321f5c98cdedc98a7db0
Shaw <lsk.boy.f@gmail.com>
2021-05-10 02:22:36 +0800

    添加冒泡排序的kotlin版本

--- app/src/main/java/org/lstec/algorithm/sort/BubbleSortKt.kt
+++ app/src/main/java/org/lstec/algorithm/sort/BubbleSortKt.kt
@@ -1,4 +1,23 @@
 package org.lstec.algorithm.sort
 
 class BubbleSortKt {
+    companion object {
+        fun sort(arr: IntArray?): IntArray? {
+            return if (arr == null || arr.size == 1) {
+                arr
+            } else {
+                for (i in 0 until arr.size - 1) {
+                    for (j in 0 until arr.size - 2 - i) {
+                        if (arr[j] > arr[j + 1]) {
+                            val temp = arr[j + 1]
+                            arr[j + 1] = arr[j]
+                            arr[j] = temp
+                        }
+                    }
+                }
+
+                arr
+            }
+        }
+    }
 }
--- app/src/test/java/org/lstec/algorithm/sort/BubbleSortKtTest.kt
+++ app/src/test/java/org/lstec/algorithm/sort/BubbleSortKtTest.kt
@@ -1,5 +1,26 @@
 package org.lstec.algorithm.sort
 
 import org.junit.Assert.*
+import org.junit.Test
 
-class BubbleSortKtTest
+class BubbleSortKtTest {
+    @Test
+    fun test(): Unit {
+        assertArrayEquals(
+            null,
+            BubbleSortKt.sort(null)
+        )
+        assertArrayEquals(
+            listOf(1, 2, 3, 4, 5).toIntArray(),
+            BubbleSortKt.sort(listOf(1, 2, 3, 4, 5).toIntArray())
+        )
+        assertArrayEquals(
+            listOf(1, 2, 3, 4, 5).toIntArray(),
+            BubbleSortKt.sort(listOf(1, 3, 2, 4, 5).toIntArray())
+        )
+        assertArrayEquals(
+            listOf(1, 3, 4, 4, 5).toIntArray(),
+            BubbleSortKt.sort(listOf(1, 4, 3, 4, 5).toIntArray())
+        )
+    }
+}
