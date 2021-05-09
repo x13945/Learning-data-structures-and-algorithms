78f17aaf966bdeae14dc445347fbdbf0d72c2747
Shaw <lsk.boy.f@gmail.com>
2021-05-10 02:26:36 +0800

    优化冒泡排序

--- app/src/main/java/org/lstec/algorithm/sort/BubbleSort.java
+++ app/src/main/java/org/lstec/algorithm/sort/BubbleSort.java
@@ -7,12 +7,17 @@ public class BubbleSort {
         }
 
         for (int i = 0; i < data.length; i++) {
+            boolean swap = false;
             for (int j = 0; j < data.length - i - 1; j++) {
                 if (data[j] > data[j + 1]) {
                     int temp = data[j];
                     data[j] = data[j + 1];
                     data[j + 1] = temp;
+                    swap = true;
                 }
+            }
+            if (!swap) {
+                return data;
             }
         }
         return data;
--- app/src/main/java/org/lstec/algorithm/sort/BubbleSortKt.kt
+++ app/src/main/java/org/lstec/algorithm/sort/BubbleSortKt.kt
@@ -7,12 +7,19 @@ class BubbleSortKt {
                 arr
             } else {
                 for (i in 0 until arr.size - 1) {
+                    // 如果有元素在冒泡的过程中，没有数据交换，说明该数组已经排好顺序
+                    // 最好的时间复杂度就是这个时候：O(n)
+                    var swap = false
                     for (j in 0 until arr.size - 2 - i) {
                         if (arr[j] > arr[j + 1]) {
                             val temp = arr[j + 1]
                             arr[j + 1] = arr[j]
                             arr[j] = temp
+                            swap = true
                         }
+                    }
+                    if (!swap) {
+                        return arr
                     }
                 }
 
